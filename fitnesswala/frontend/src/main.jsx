cd4e5aef62dd4c2208d766e0d77254d926701d36
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-11-02 17:55:26 +0530

    Update main.jsx

--- fitnesswala/frontend/src/main.jsx
+++ fitnesswala/frontend/src/main.jsx
@@ -3,16 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// ✅ Import your context providers
-import { AuthProvider } from "./contexts/AuthContext";
-import { UserProvider } from "./contexts/UserContext";
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
